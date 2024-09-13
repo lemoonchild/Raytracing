b34703c0858dd30824b319057f62741274001a78
lemoonchild <ncastrom21@gmail.com>
2024-09-12 20:50:21 -0600

    feat: first class

--- diorama-mc/src/main.rs
+++ diorama-mc/src/main.rs
@@ -1,3 +1,163 @@
+use minifb::{Key, Window, WindowOptions};
+use core::f32;
+use std::time::Duration;
+use nalgebra_glm::Vec3;
+
+use std::f32::consts::PI; 
+use std::f32::INFINITY; 
+
+mod framebuffer;
+use framebuffer::Framebuffer; 
+
+mod sphere;
+use sphere::Sphere; 
+
+mod ray_intersect;
+use ray_intersect::{RayIntersect, Intersect, Material};
+
+mod color;
+use color::Color; 
+
+mod camera;
+use camera::Camera; 
+
+
+pub fn cast_ray(ray_origin: &Vec3, ray_direction: &Vec3, objects: &[Sphere]) -> Color {
+
+    let mut intersect = Intersect::empty(); 
+    let mut zbuffer = INFINITY; 
+
+    for object in objects {
+
+        let i = object.ray_intersect(ray_origin, ray_direction);
+
+        if i.is_intersecting && i.distance < zbuffer {
+            zbuffer = i.distance;
+            intersect = i; 
+        }  
+    }
+
+    if !intersect.is_intersecting {
+        return Color::new(4, 12, 36); 
+    }
+
+    let diffuse = intersect.material.diffuse; 
+
+    diffuse
+}
+
+pub fn render(framebuffer: &mut Framebuffer, objects: &[Sphere], camera: &Camera) {
+    let width = framebuffer.width as f32;
+    let height = framebuffer.height as f32;
+    let aspect_ratio = width / height;
+    let fov = PI/3.0; 
+    let perspective_scale = (fov / 2.0).tan();
+
+    for y in 0..framebuffer.height {
+        for x in 0..framebuffer.width {
+            // Map the pixel coordinate to screen space [-1, 1]
+            let screen_x = (2.0 * x as f32) / width - 1.0;
+            let screen_y = -(2.0 * y as f32) / height + 1.0;
+
+            // Adjust for aspect ratio
+            let screen_x = screen_x * aspect_ratio * perspective_scale;
+            let screen_y = screen_y * perspective_scale; 
+
+            // Calculate the direction of the ray for this pixel
+            let ray_direction = &Vec3::new(screen_x, screen_y, -1.0).normalize();
+
+            let rotated_direction = camera.basis_change(&ray_direction); 
+
+            let pixel_color = cast_ray(&camera.eye, &rotated_direction, objects);
+
+            // Draw the pixel on screen with the returned color
+            framebuffer.set_current_color(pixel_color.to_hex());
+            framebuffer.point(x, y);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let window_width = 800;
+    let window_height = 600;
+
+    let framebuffer_width = 800;
+    let framebuffer_height = 600;
+
+    let frame_delay = Duration::from_millis(0);
+
+    let mut framebuffer = Framebuffer::new(framebuffer_width, framebuffer_height);
+
+    let mut window = Window::new(
+        "Gráficas - Diorama Minecraft",
+        window_width,
+        window_height,
+        WindowOptions::default(),
+    )
+    .expect("Failed to create window");
+
+    window.set_position(100, 100);  
+    window.update();
+
+    framebuffer.set_background_color(0x333355);
+
+    let rubber = Material{
+        diffuse: Color::new(80, 0, 0),
+    }; 
+
+    let ivory = Material {
+        diffuse: Color::new(100, 100, 100)
+    };
+
+    let objects = [
+        Sphere {
+            center: Vec3::new(-0.2, 0.0, 2.0),
+            radius: 0.2, 
+            material: ivory,
+        },
+        Sphere {
+            center: Vec3::new(0.0, 0.0, 0.0),
+            radius: 1.0,
+            material: rubber,
+        }
+    ];
+
+
+    let mut camera = Camera::new(
+        Vec3::new(0.0, 0.0, 5.0),
+        Vec3::new(0.0, 0.0, 0.0),
+        Vec3::new(0.0, 1.0, 0.0),
+    );
+
+
+    let rotation_speed = PI/20.0; 
+
+    while window.is_open() {
+        if window.is_key_down(Key::Escape) {
+            break;
+        }
+
+        // camera orbit controls
+        if window.is_key_down(Key::Left) {
+            camera.orbit(rotation_speed, 0.0);
+        }
+        if window.is_key_down(Key::Right) {
+            camera.orbit(-rotation_speed, 0.0);
+        }
+        if window.is_key_down(Key::Up) {
+            camera.orbit(0.0, -rotation_speed);
+        }
+        if window.is_key_down(Key::Down) {
+            camera.orbit(0.0, rotation_speed);
+        }
+
+        framebuffer.clear();
+        render(&mut framebuffer, &objects, &mut camera);
+
+        window
+            .update_with_buffer(&framebuffer.buffer, framebuffer_width, framebuffer_height)
+            .unwrap();
+
+        std::thread::sleep(frame_delay);
+    }
 }
---
+++ diorama-mc/src/sphere.rs
@@ -0,0 +1,42 @@
+use nalgebra_glm::Vec3; 
+use crate::ray_intersect::{Intersect, RayIntersect, Material};
+
+pub struct Sphere {
+    pub center: Vec3,
+    pub radius: f32,
+    pub material: Material,
+}
+
+impl RayIntersect for Sphere {
+    fn ray_intersect(&self, ray_origin: &Vec3, ray_direction: &Vec3) -> Intersect {
+        // Vector from the ray origin to the center of the sphere
+        let oc = *ray_origin - self.center;
+
+        // Coefficients for the quadratic equation
+        let a = ray_direction.dot(ray_direction);
+        let b = 2.0 * oc.dot(ray_direction);
+        let c = oc.dot(&oc) - self.radius * self.radius;
+
+        // Calculate the discriminant
+        let discriminant = b * b - 4.0 * a * c;
+
+        // If the discriminant is greater than 0, the ray intersects the sphere
+        if discriminant > 0.0 {
+            // Calculate the nearest point of intersection
+            let t = (-b - discriminant.sqrt()) / (2.0 * a);
+            if t > 0.0 {
+                // Compute intersection point, normal at the intersection, and distance from the ray origin
+                //let point = ray_origin + ray_direction * t;
+                //let normal = (point - self.center).normalize();
+                let distance = t;
+        
+                return Intersect::new(distance, self.material);
+            }
+        }
+        
+        // If no intersection, return an empty intersect
+        Intersect::empty()
+        
+
+    }
+}
